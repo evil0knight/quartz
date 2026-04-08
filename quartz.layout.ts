59ed2867317e58ded906a612f90969ac8b94ff8a
Ao Yang <1744393991@qq.com>
2026-04-08 19:53:21 +0800

    美化首页样式、评论区配置、站点标题

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        repo: "evil0knight/Basic_learning_record",
+        repoId: "R_kgDORm9zUw",
+        category: "Announcements",
+        categoryId: "DIC_kwDORm9zU84C6Xrv",
+        mapping: "pathname",
+        strict: false,
+        reactionsEnabled: true,
+        inputPosition: "bottom",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
